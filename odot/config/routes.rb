b2d866621399529d07bd3eeeda61dcbaea252079
Matty Gabbitas <mattytv11@gmail.com>
2015-04-17 16:00:20 +0100

    add todo items controller

--- odot/config/routes.rb
+++ odot/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :todo_lists
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :todo_lists do
+    resources :todo_items
+  end
 
-  # You can have the root of your site routed with "root"
    root 'todo_lists#index'
 
   # Example of regular route:
